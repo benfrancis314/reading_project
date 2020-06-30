a21b544bcfab6f44b412bf2c03f748a6394201a1
Ben Francis <ben.francis@xprincipia.com>
2020-06-30 16:44:07 -0500

    cleaned up a bit; added semicolons

--- extension_example/content_script.js
+++ extension_example/content_script.js
@@ -1,102 +1,58 @@
-var container = $("p:first")
-var start = 0
-var end = 0
-var velocity = 1
-var timer = 0
-var speed = 10
-var init = 0
-var scrollType = "velocity"
+var container = $("p:first");
+var start = 0;
+var end = 0;
+var velocity_mode = 1;
+var timer = 0;
+var speed = 10;
+var init = 0;
+var scrollType = "velocity_mode";
 
 $(function () {
     $("p").click(function () {
 		container = $(this);
-		start = 0
-		end = container.text().indexOf(". ", start)
-		highlight(container, start, end)
-		init = 1
+		start = 0;
+		end = container.text().indexOf(". ", start);
+		highlight(container, start, end);
+		init = 1;
 	});
 });
 
+// This should be used elsewhere
 function initContentScript() {
-	// innerHTML of this container will be changing
-    // let container = $("#Methods_and_thanks").next();
-    end = container.text().indexOf(". ", start)
-	highlight(container, start, end)
-}
+    end = container.text().indexOf(". ", start);
+	highlight(container, start, end);
+};
 function moveUp() {
-	// This is actually more non-trivial than I thought
-	len = container.text().length
-	end = start -2
-	rev = container.text().split("").reverse().join("")
+	len = container.text().length;
+	end = start -2;
+	rev = container.text().split("").reverse().join("");
 	if (rev.indexOf(" .", len-end) > 0) {
 		start = len - rev.indexOf(" .", len-end)
 	} else { 
-		// console.log("start<0 did trigger")
 		start = 0 
-	}
-	if (start < 0) {start = 0}
-	// console.log("len"+len)
-	// console.log("end"+end)
-	// console.log("start"+start)
+	};
+	if (start < 0) {start = 0};
     if (end < 0) {
-		// console.log("end<0 did trigger")
 		container = container.prev()
-		// console.log(container.text())
 		len = container.text().length
-		// console.log("len"+len)
 		end = len
-		// console.log("end"+end)
 		rev = container.text().split("").reverse().join("")
-		// console.log("rev"+rev)
 		if (rev.indexOf(" .", len-end) > 0) {
 			start = len - rev.indexOf(" .", len-end)
 		} else { 
-			// console.log("start<0 did trigger")
 			start = 0 
-		}
-		// console.log("start"+start)
-    } 
-    highlight(container, start, end)
+		};
+    } ;
+    highlight(container, start, end);
 }
 
-// function sleep(milliseconds) {
-// 	console.log("sleep started");
-// 	const date = Date.now();
-// 	let currentDate = null;
-// 	do {
-// 	  currentDate = Date.now();
-// 	} while (currentDate - date < milliseconds);
-// 	console.log("sleep ended")
-// }
-// function sleep(ms) {
-// 	return new Promise(resolve => setTimeout(resolve, ms));
-// }
-
 function moveDown() {
-	velocity = 1
-	if (scrollType == "manual") {
+	velocity = 1;
+	if (scrollType == "manual_mode") {
 		moveDownManual()
 	} else {
-		// timer_speed = speed * 50
-		// while (velocity) {
-		// for (i=0;i<3;i++) {
-		// 	console.log("start")
 		if (!timer) {timer = setInterval(moveDownManual, 500)}
-			// timer = setInterval(moveDownManual, 500)
-		// }
-		// 	moveDownManual()
-		// 	sleep(timer_speed)
-		// 	console.log("stop")
-		// 	// moveDownManual()
-		// }
-		// // for (i = 0; i < 2; i++) {
-			
-		// // }
-		// moveDownManual()
-		// sleep(1000)
-		// moveDownManual()
-		// sleep(2000).then(() => { moveDownManual(); });
-	}
+	};
 }
 
 function moveDownManual() {
@@ -106,19 +62,19 @@ function moveDownManual() {
 		end = container.text().indexOf(". ", start)
 		highlight(container, start, end)
 		init = 1
-	}
-	len = container.text().length
-    start = end + 2
-    end = container.text().indexOf(". ", start+2)
-	if (end < 0) { end = len }
+	};
+	len = container.text().length;
+    start = end + 2;
+    end = container.text().indexOf(". ", start+2);
+	if (end < 0) { end = len };
     if (start >= len) {
 		container = container.next()
         start = 0
 		end = container.text().indexOf(". ", start+2)
 		if (end < 0) { end = container.text().length}
-    } 
-    highlight(container, start, end)
-}
+    } ;
+    highlight(container, start, end);
+};
 
 /**
 Highlight a portion container.text(), from start_off to end_off (exclusive).
@@ -136,8 +92,7 @@ function highlight(container, start_off, end_off) {
 	}], {
 		className: 'marked'
 	});
-	// $(".marked").animate({color: '#c9daf8 !important'});
-}
+};
 
 function readListener() {
 	document.addEventListener('keydown', function(evt) {
@@ -147,29 +102,22 @@ function readListener() {
 		switch (evt.keyCode) {
             // Up
 			case 37:
-				console.log("up")
-                moveUp()
+                moveUp();
                 break;
             // Down
 			case 39:
-				// sleep(500).then(() => { setTimeout(moveDownManual(),0) });
-                moveDown()
+                moveDown();
 				break;
 			// D (Increase velocity)
 			case 68:
-				// alert(speed)
-				// if (speed >= 0) { speed -= 2 }s
-				speed -= 2
-				// alert(speed)
+				speed -= 2;
 			// S (Slow velocity)
 			case 83: 
 			// T (toggle scroll type)
-				// alert(speed)
-				// console.log(speed)
-				speed += 2
+				speed += 2;
 			case 84:
-				if (scrollType == "velocity") { scrollType == "manual" }
-				else if (scrollType == "manual") { scrollType == "velocity" }
+				if (scrollType == "velocity_mode") { scrollType == "manual_mode" }
+				else if (scrollType == "manual_mode") { scrollType == "velocity_mode" };
 				break;
 			default:
                 break;
@@ -181,14 +129,9 @@ function readListener() {
 		  return true;
 		}
 		if (velocity == 1) { 
-			// velocity = 0
 			clearInterval(timer)
 		}
     }, false);
-}
+};
 
-
-
-
-// initContentScript();
 readListener();
