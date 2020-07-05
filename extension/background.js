a86295773272cbbcd1428769c2deb9213c43a323
Stephen Joe Jonany <sjonany@gmail.com>
2020-07-05 12:52:06 -0700

    Refactored content_script to use the new tracker class. (#15)
    * Refactored content_script to use the new tracker class.
    Tested.
    - If not tracking
      - Move back doesn't do anything
      - Move start points to the first paragraph
    - If tracking
      - Move back if first sentence doesn't do anything
      - Move next if last sentence doesn't do anything.
    
    * Refactored content_script to use the new tracker class.
    Tested.
    - If not tracking
      - Move back doesn't do anything
      - Move start points to the first paragraph
    - If tracking
      - Move back if first sentence doesn't do anything
      - Move next if last sentence doesn't do anything.

--- extension/background.js
+++ extension/background.js
@@ -52,6 +52,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
             "third_party/jquery.mark-8.11.1.min.js",
             "third_party/jquery-ui.1.12.1.min.js",
             "third_party/readability-1.7.1.js",
+            "lib/tracker.js",
             "content_script.js"
         )
         .then(s => s.injectCss("content.css"));
