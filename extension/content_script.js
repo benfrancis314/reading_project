dab913c67da27e0d1f254ba657b6e49032dc2288
Ben Francis <ben.francis@xprincipia.com>
2020-07-07 09:49:05 -0500

    fixed nits

--- extension/content_script.js
+++ extension/content_script.js
@@ -21,7 +21,7 @@ var speed_bias = 500; // Minimum amount of speed spent on each sentence (in mill
 var speed_adj = 0; // Speed after it has been adjusted by sentence length
 // If the screen is currently scrolling. If it is, pause the tracker.
 var isScrolling = false;
-var currentStyle = "markedTwo"; // TEMPORARY global variable, just for style experimentation. Will get rid of later
+var currentStyle = "markedBoxShadow"; // TEMPORARY global variable, just for style experimentation. Will get rid of later
 
 /*
 To do:
