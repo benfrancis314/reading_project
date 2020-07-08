3711f3116f5d36c6ce27aa1085bbc7662db0dbf4
Ben Francis <ben.francis@xprincipia.com>
2020-07-08 12:43:19 -0500

    Revert "removed regex stuff temporarily"
    This reverts commit 7e44934059baa6ae5b7772181eba460d216e8977.
    
    Reverting master to version before last commit, which was never
    reviewed.

--- extension/lib/tracker.js
+++ extension/lib/tracker.js
@@ -167,12 +167,10 @@ class Tracker {
     - end (int). Exclusive index to the end of the sentence within text that starts at 'start' 
     */
     getSentenceEnd(text, start) {
-        // REGEX EXPERIMENTATION FOR LATER DEALING WITH EDGE CASES
-        // let reEnd = /[(\..)]/;
+        // let reEnd = /\../;
         // let sentence_end = text.slice(start).search(reEnd);
-        // let end = sentence_end + start;
+        // end = sentence_end + start;
         let end = text.indexOf(". ", start);
-        console.log(end);
         if (end < 0) {
             end = text.length;
         }
