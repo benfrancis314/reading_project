9f96bd7e2e4c1ca4533c56b67514cd53ed6dbd34
Ben Francis <ben.francis@xprincipia.com>
2020-07-15 15:20:54 -0500

    added better documentation to display class

--- extension/lib/display.js
+++ extension/lib/display.js
@@ -7,19 +7,22 @@ if (window[namespace] === true) {
     window[namespace] = true;
 }
 
-var avg_read_speed = 200; // WPM
-var avg_letters_per_word = 6; // 4.79; just from Quora, add a space after each word
-var avg_words_per_sentence = 25 // from http://www.hearle.nahoo.net/Academic/Maths/Sentence.html
+const avg_read_speed = 200; // WPM
+const avg_letters_per_word = 6; // 4.79; just from Quora, add a space after each word
 
+/*
+Creates a display overlaid on current web page that displays
+the estimated time remaining to read and the current speed of 
+the auto-read mode in WPM. 
+*/
 class Display {
     /*
-    Initialize a display that [ -- is not tracking anything -- ].
+    Initialize a display with total time remaining and initial speed (~200 WPM).
 
     Parameters:
     - readableDomIds: string[]. List of dom IDs that contain readable content.
+    - speeed: int. Value proportional to the speed of auto-read mode, before adjustment by sentence length. 
     */
-
-
     constructor(readableDomIds, speed) {
         this.readableDomIds = readableDomIds; // Used to calc initial reading time
         this.html = null;
@@ -31,7 +34,10 @@ class Display {
         this.createDisplay(readableDomIds);
     }
 
-    defineHtml() { // One liner
+    /*
+    Defines html code for display to be inserted into web page. 
+    */
+    defineHtml() {
         this.html = `
             <div id="displayContainer">
                 <div id="timerContainer">
@@ -48,23 +54,37 @@ class Display {
             `;      
     }
 
+    /*
+    Inserts display HTML into webpage. 
+    */
     createDisplay(readableDomIds) {
         document.getElementById(readableDomIds[0]).innerHTML = this.html;
-        document.getElementById("displayContainer").style.opacity = 1;
+        document.getElementById("displayContainer").style.opacity = 1; // For smoother transition
     }
+
+    /* 
+    Params: 
+    - List of container IDs on web page (string[])
+    Return: 
+    The total time remaining to read document, based on avg letters/word and lowball estimate of WPM (int)
+    */
     initTimer(readableDomIds) {
         let total_char = 0;
-        for (var section in readableDomIds) {
+        let avg_letters_per_word = 6; // Actual is 4.79 (from Quora), add a space after each word to get 6. 
+        let avg_read_speed = 200; // Low ball estimate, from Rayner (in WPM)
+        for (var section in readableDomIds) { // Get total_char of all containers in web page
             let elem = document.getElementById(readableDomIds[section]);
             let text_len = elem.innerText.length;
             total_char = total_char + text_len;
         }
-        const avg_letters_per_word = 6; // 4.79; just from Quora, add a space after each word
-        const avg_read_speed = 200; // wpm
         let total_words = total_char/avg_letters_per_word;
         let time_remaining = total_words/avg_read_speed; // in minutes
         return Math.ceil(time_remaining);
     }
+    
+    /*
+    Updates reading timer based on containers after current tracker. 
+    */
     updateTimer(readableDomIds, containerId) { // Call everytime you get to new paragraph
         let total_char = 0;
         for (var section in readableDomIds.slice(containerId)) {
@@ -78,29 +98,27 @@ class Display {
         document.getElementById("timerNumber").innerHTML = this.time_remaining;
     }
 
-    getHtml() {
-        return this.html;
-    }
-    getSpeed() {
-        return this.reading_speed;
-    }
-    getTime() {
-        return this.time_remaining;
-    }
-    calcTime() {
-        // Put reading time calculation here
-        // USE READABLE DOM IDS passed in as param
-        this.time_remaining = 1;
-    }
+    /*
+    Returns: 
+    - Initial speed in WPM, based on speed parameter used by tracker (int)
+    */
     initSpeed(speed) {
+    /* REASONING: 
+        Avg sentence has 25 words, avg word has 5 letters -> avg letters per sentence ~ 125
+        speed_adj is usually 20*125 + 500 -> 3000 ms [base_speed * avg_letters_per_sentence + speed_bias]
+        So 25 words/3s -> ~400 WPM. Since speed = 20, and we want it to be 400, so we multiply by 20. 
+        Note this is rampant with estimations/is kinda bs. But good enough for hypothesis testing. 
+        In part, trickiness comes from speed's dependcy on sentence length. 
+        */
         console.log(speed);
         return speed * 20
-            // Avg sentence has 25 words. -> 25 words/second -> 1500 WPM
-            // speed_adj is usually 20*125 + 500 -> 3000. So 25 words/3s -> ~400 WPM. (Note this is rampant with estimations)
     }
+
+    /* 
+    Update display speed after speed is changed by user
+    */
     updateSpeed(speed) {
-        // Put reading time calculation here
-        this.reading_speed = 800 - (20*speed) // This is a completely fake equation; just for show for now
+        this.reading_speed = 800 - (20*speed) // Completely made up eq, reasonable enough for testing though
         document.getElementById("speedNumber").innerHTML = this.reading_speed;
     }
 }
