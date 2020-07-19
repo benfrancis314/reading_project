6526e914c66478fb1b86e68576127af8197ea66c
Stephen Joe Jonany <sjonany@gmail.com>
2020-07-18 21:22:00 -0700

    Changed all readableDomIds to readableDomEls. (#51)
    Why:
    - More efficient. $("") actually does dom lookup each time.
    - Actually needed for the on/off change later on. For some reason re-invoking $("<id>") during callback doesn't capture the click event handlers anymore, so I need consistent direct pointers from now on.
    
    No behavior change, just pure refactor.

--- extension/lib/doc.js
+++ extension/lib/doc.js
@@ -13,14 +13,14 @@ Note: Called "Doc" because Document is already used in js.
 */
 
 class Doc {
-    constructor(readableDomIds) {
-        this.readableDomIds = readableDomIds; // string[]; List of all container IDs
+    constructor(readableDomEls) {
+        this.readableDomEls = readableDomEls; // $[]; List of all jquery readable containers.
         this.containerId = null; // str; ID of current container
         this.termFreq = null; // { str : int } , Frequency of terms in document. Set in calcTotalWords function
-        this.total_words = this.calcTotalWords(readableDomIds); // int; Total number of words in document;
+        this.total_words = this.calcTotalWords(readableDomEls); // int; Total number of words in document;
         this.keywords = this.setKeyWords(this.termFreq); // string[] keywords of document
         this.container_sentences_map = null; // int[], Index[i] is # of sentneces in ith container, This is set by calcTotalSentences()
-        this.total_sentences = this.calcTotalSentences(readableDomIds);
+        this.total_sentences = this.calcTotalSentences(readableDomEls);
     };
 
     // Returns: Total words in doc (int)
@@ -33,15 +33,6 @@ class Doc {
         return this.keywords;
     }
 
-    // TO DO: REFACTOR; redundant with an identical function in tracker.js
-    // Returns: Container given container ID (jQuery element)
-    getContainer(containerId) {
-        if (containerId === null || containerId < 0 || containerId >= this.readableDomIds.length) {
-            throw `Invalid ${containerId}, should be [0, ${this.readableDomIds.length})`;
-        }
-        return $("#" + this.readableDomIds[containerId]);
-    }
-
     // Returns: Container-sentences map; tells how many sentences are in each container
     getContainerSentencesMap() {
         return this.container_sentences_map;
@@ -51,12 +42,12 @@ class Doc {
     Returns: Total words in document (int)
     Calculates total words in the document
     */
-    calcTotalWords(readableDomIds) {
+    calcTotalWords(readableDomEls) {
         var termFreq = {};
         var total_words = 0;
         var stop_words = new Set(["succeeded", "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot ", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself ", "him", "himself", "his", "how", "however", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off ", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan’t", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]);
-        for (var section in readableDomIds) {
-            let text = this.getContainer(section).text();
+        for (var section in readableDomEls) {
+            let text = readableDomEls[section].text();
             let wordRegex = /\b\w+\b/g;
             let wordList = text.match(wordRegex);
             for (var i in wordList) {
@@ -80,11 +71,11 @@ class Doc {
     Params: Current container ID
     Returns: # of sentences in document, starting from current container
     */
-    calcTotalSentences(readableDomIds) {
+    calcTotalSentences(readableDomEls) {
         let total_sentences = 0;
         let container_sentences_map = []; // For each container, add # of sentences in it
-        for (var section in readableDomIds) {
-            let text = this.getContainer(section).text();
+        for (var section in readableDomEls) {
+            let text = readableDomEls[section].text();
             let start = 0;
             let end = 0;
             var container_sentences = 0;
