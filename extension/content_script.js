df2645d4c4e96f26583a1ccac1b26c860ca17180
Ben Francis <ben.francis@xprincipia.com>
2020-07-22 22:20:15 -0500

    fixed problem of customizatino buttons not reloading when changed; related to github issue 101 and animating the fading elements

--- extension/content_script.js
+++ extension/content_script.js
@@ -313,12 +313,25 @@ function fadeTracker(fadeMs) {
 Stop all animations related to fading.
 */
 function stopFadeTracker() {
-	let sentence_el = $("."+sentenceClass)
-	let keywords_el = $("."+keywordClass)
+	let sentenceStyle = trackerStyle.getSentenceStyle();
+	let keywordStyle = trackerStyle.getKeywordStyle(); 
+
+	let sentence_el = $("."+sentenceStyle)
+	let keywords_el = $("."+keywordStyle)
 	let sentence_color = sentence_el.css('backgroundColor');
 	let keywords_color = keywords_el.css('backgroundColor');
-	sentence_el.stop().animate({'background-color': sentence_color});
-	keywords_el.stop().animate({'background-color': keywords_color});
+	sentence_el.stop().animate({'background-color': sentence_color},
+		{	duration: 300,
+			complete: function() { 
+				sentence_el.removeAttr('style');
+		}
+		});
+	keywords_el.stop().animate({'background-color': keywords_color},
+		{	duration: 300,
+			complete: function() { 
+				keywords_el.removeAttr('style');
+		}
+		});
 }
 
 
