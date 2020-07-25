2e03ebcf514ed81d6d65c6b7fa5f8ae4d5689a79
benfrancis314 <47435096+benfrancis314@users.noreply.github.com>
2020-07-25 11:22:29 -0500

    Added Google Analytics (#114)
    * added security meta tags, console.log to see when it is updated
    
    * testing changes to see if website updates
    
    * fixed website problems; content security policy is handled on AWS now (using Lambda@Edge functions)
    
    Co-authored-by: Ben Francis <ben.francis@xprincipia.com>

--- website/app.js
+++ website/app.js
@@ -1,1 +1,7 @@
-console.log("New version is up");
+// Keep all scripts here/not inline. Prevents need for inline scripting, a security risk. 
+
+// Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-173450926-1');
