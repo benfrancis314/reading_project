48319e9ed0a62d0aafe78e7f849f840d7c734160
benfrancis314 <47435096+benfrancis314@users.noreply.github.com>
2020-07-27 16:51:07 -0500

    Added term document frequency map to persistent settings (#117)
    * added security meta tags, console.log to see when it is updated
    
    * added term document frequency map as persistent setting
    
    * added TODO, removed comment
    
    Co-authored-by: Ben Francis <ben.francis@xprincipia.com>

--- extension/lib/doc.js
+++ extension/lib/doc.js
@@ -7,6 +7,8 @@ if (window[namespace] === true) {
     window[namespace] = true;
 }
 
+var settings = window.settings;
+
 // If the number of sentences in the doc exceeds this, mark the document as unreadable.
 const MAX_NUM_SENTENCE = 1000;
 
@@ -160,9 +162,11 @@ class Doc {
     Calculates total words in the document
     */
     calcTotalWords(readableDomEls) {
+        var self = this;
         var termFreq = {};
         var total_words = 0;
         var stop_words = new Set(["succeeded", "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot ", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself ", "him", "himself", "his", "how", "however", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off ", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan’t", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]);
+        var termDocumentFreq = null;
         for (var section in readableDomEls) {
             let text = readableDomEls[section].text();
             let wordRegex = /\b\w+\b/g;
@@ -180,8 +184,27 @@ class Doc {
             };
             if ( wordList) { total_words += wordList.length; };
         }
+        // Update term document frequency object: 
+        settings.getTermDocumentFreq(function(settingsTermDocumentFreq) {
+            termDocumentFreq = settingsTermDocumentFreq;
+            self.updateTermDocumentFreq(termFreq, termDocumentFreq, settings.setTermDocumentFreq);
+        });
+
         this.termFreq = termFreq; // Set class attribute "termFreq"
         return total_words // Set total words for use elsewhere (like Display)
+    }
+
+    updateTermDocumentFreq(termFreq, termDocumentFreq, cb) {
+        // For each unique word in doc; add to termDocumentFreq dict or add one to its frequency
+        for (const word in termFreq) {
+            if (word in termDocumentFreq) {
+                termDocumentFreq[word]++
+            } else {
+                termDocumentFreq[word] = 1;
+            }
+        }
+        cb(termDocumentFreq);
+        return;
     }
 
     /*
