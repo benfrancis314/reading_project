3a7cb4277bc590bc589f65a58d736c054344f5f0
Stephen Jonany <sjonany@gmail.com>
2020-07-28 20:40:08 -0700

    Turn off debugging

--- extension/content_script.js
+++ extension/content_script.js
@@ -11,7 +11,7 @@ if (window[namespace] === true) {
 
 // If true, all debugging statements would show.
 // TODO: Use a proper logging library.
-window.DEBUG = true;
+window.DEBUG = false;
 window.debug = function(str) {
 	if (DEBUG) {
 		console.log("DEBUG: " + str);
