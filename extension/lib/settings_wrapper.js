cbd333c220af7be296f214f4848da251fec49c38
Stephen Jonany <sjonany@gmail.com>
2020-07-31 21:01:23 -0700

    Implement get and set highlights. Next CL will incorporate this to the UI.

--- extension/lib/settings_wrapper.js
+++ extension/lib/settings_wrapper.js
@@ -27,16 +27,57 @@ const settingKey = {
 	DOCUMENT_FREQ: "termDocumentFreq",
 
 	// URLs that user has visited. set<String>
-	VISITED_URLS: "urls"
+	VISITED_URLS: "urls",
+
+	// Saved persistent highlights
+	// Each doc's highlight will have its own chrome setting key. Please see getHighlightKey(docUrl)
+	// Each chrome setting value (a single doc's highlights) is int[], the highlighted sentence IDs. 
+	HIGHLIGHTS: "highlights"
 };
 
 const trackerSettingKey = {
 	KEYWORD: "keyword"
-}
+};
 const trackerSettingValue = {
 	LIGHT: "light",
 	BRIGHT: "bright",
 	OFF: "off"
+};
+
+// See https://docs.google.com/document/d/122o4vYiK3RLmYCoT_tL3bzmPzPUb0Q8DMIu2BgaT07U/edit
+// For why we set these limits.
+// Max number of highlights for a single doc.
+const MAX_HIGHLIGHT_PER_DOC = 20;
+// Max allowable documents w/ persistent highlight.
+const MAX_HIGHLIGHTED_DOC = 100;
+// All saved highights' chrome setting keys begin with this.
+// Used in getHighlightKey()
+const HIGHLIGHT_KEY_PREFIX = `highlight_`;
+
+/*
+Return a 32-bit hash of a string.
+https://stackoverflow.com/a/7616484/4143394
+*/
+function hashString(str) {
+	let hash = 0, i, chr;
+    for (i = 0; i < str.length; i++) {
+      chr   = str.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+/*
+Parameters:
+- docUrl: string. E.g. "www.wikipedia.org"
+Return:
+- string: chrome setting key for the highlights of this document.
+E.g "highlight_234123423"
+*/
+function getHighlightKey(docUrl) {
+	let hash = hashString(docUrl);
+	return HIGHLIGHT_KEY_PREFIX + hash;
 }
 
 /*
@@ -155,6 +196,52 @@ class Settings {
 		});
 	}
 
+	/*
+	Save persistent highlights for url.
+	Parameters:
+	- url: string.
+	- sentenceIds: set<int>.
+	*/
+	setHighlights(url, sentenceIds) {
+		if (sentenceIds.size > MAX_HIGHLIGHT_PER_DOC) {
+			throw `You hit the max limit of ${MAX_HIGHLIGHT_PER_DOC} highlights in a document.`
+				+ ` Please manually delete some before adding new ones.`;
+		}
+
+		let key = getHighlightKey(url);
+		// Remove from setting key to make space.
+		if (sentenceIds.size == 0) {
+			delete this.settings[key];
+			chrome.storage.local.remove(key, function() {
+				if (chrome.runtime.lastError) {
+					console.log("Failed to clear highlighted doc: " + chrome.runtime.lastError);
+					return;
+				}
+			});
+			return;
+		}
+		
+		// Handle "too many documents" case.
+		if (!(key in this.settings[key]) && this.getNumHighlightedDocs() >= MAX_HIGHLIGHTED_DOC) {
+			// TODO: No easy way for user to figure out which documents have highlights :(
+			// A possible soln is yet another UI to see bookmarked docs that have highlights.
+			throw `You hit the max limit of ${MAX_HIGHLIGHTED_DOC} highlighted documents.`
+				+ ` Please manually delete all highlights of some documents before adding new ones.`
+				+ ` (Or write a feature request to the devs)`;
+		}
+
+		let sentenceIdArr = Array.from(sentenceIds);
+		// For both new and existing cases, update in memory and storage, 
+		this.settings[key] = sentenceIdArr;
+		chrome.storage.local.set({[key]: sentenceIdArr}, function() {
+			if (chrome.runtime.lastError) {
+				console.log("Failed to save highlight: " + chrome.runtime.lastError);
+				return;
+			}
+		});
+	}
+
+
 	// See SPEED
 	getSpeed(cb) {
 		let key = settingKey.SPEED;
@@ -201,6 +288,43 @@ class Settings {
 			this.settings[key] = {};
 		}
 		return this.settings[key];
+	}
+
+	/*
+	Parameters:
+	- url: string. The url of the document.
+	- numSentence: int. The number of sentences in current document.
+	   This is used to check if the document has changed.
+	   If it has, we remove only highlighted sentence IDs that are beyond numSentence.
+	Return:
+	- set<int>. The highlighted sentence ids.
+	See HIGHLIGHTS for more detail.
+	*/
+	getHighlights(url, numSentence) {
+		let key = getHighlightKey(url);
+		if (!(key in this.settings)) {
+			// Default highlights
+			this.settings[key] = [];
+		}
+		// TODO: Can optimize by always sorting and checking last element before filtering.
+		// Remove sentence IDs that are too big, in case document has changed.
+		this.settings[key] = this.settings[key].filter(function(numSentence) {
+			return sentenceId < numSentence;
+		});
+		return new Set(this.settings[key]);
+	}
+
+	/*
+	Return: int. The number of documents with at least one saved highlight.
+	*/
+	getNumHighlightedDocs() {
+		let count = 0;
+		for (let key in this.settings) {
+			if (key.startsWith(HIGHLIGHT_KEY_PREFIX)) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
 
